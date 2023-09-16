41242add67eb1c46668d60b327c1d4263fe5f27f
buckywebb <bucky_webb@yahoo.com>
2023-09-16 18:54:35 -0400

    Finished 'Sending a PUT Request

--- public/main.js
+++ public/main.js
@@ -1,5 +1,12 @@
 const update = document.querySelector('#update-button')
 
 update.addEventListener('click', _ => {
-    //Send PUT request here
+    fetch('/flavors', {
+        method: 'put',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            flavor: 'You have to be NUTS to order Walnuts!',
+            rating: 'TRASH!',
+        }),
+    })
 })
